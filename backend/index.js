6596fb4d94f01691946c822ca77fef5f3c94edaa
gincode <vishalkamboj9211@gmail.com>
2023-07-13 21:43:26 +0530

    done with mobile otp and dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 // Import required packages
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+//Twilio
+var sid = process.env.Sid;
+var token = process.env.Token;
+var twilionumber = process.env.number;
+var twilio = require("twilio")(sid, token);
 // Initialize the app
 const app = express();
 
@@ -10,17 +16,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Generate otp
+function generateOTP(length) {
+  const characters = "0123456789";
+  const otpArray = [];
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    const randomCharacter = characters.charAt(randomIndex);
+    otpArray.push(randomCharacter);
+  }
+
+  return otpArray.join("");
+}
+
+// twilio.messages.create({
+//   from:twilionumber,
+//   to:'+919729233981',
+//   body:otp
+
+// }).then((res)=>{
+//   console.log('message sent');
+// }).catch((err)=>{
+//   console.log(err);
+// })
 // Connect to MongoDB
-
-
-
-mongoose.set('strictQuery', true);
-  mongoose.connect('mongodb+srv://vishal:vishal1234@vibtree.lfch0kj.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log('connected to mongo'))
-    .catch((err) => {
-      console.error('failed to connect with mongo');
-      console.error(err);
-    });
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => console.log("connected to mongo"))
+  .catch((err) => {
+    console.error("failed to connect with mongo");
+    console.error(err);
+  });
 
 // Define a schema for inventory items
 const inventoryItemSchema = new mongoose.Schema({
@@ -28,11 +56,36 @@ const inventoryItemSchema = new mongoose.Schema({
 });
 
 // Define a model for inventory items
-const InventoryItem = mongoose.model('InventoryItem', inventoryItemSchema);
+const InventoryItem = mongoose.model("InventoryItem", inventoryItemSchema);
 
 // Routes
+//send mobile otp
+app.post("/api/otp", async (req, res) => {
+  const number = req.body.number;
+  const otp = generateOTP(6);
+
+  try {
+    twilio.messages
+      .create({
+        from: twilionumber,
+        to: number,
+        body: otp,
+      })
+      .then(() => {
+        res.json({ code: "1", mess: "message sent" });
+      })
+      .catch((err) => {
+        const mess = { code:"0",mess: "please verify your number with" };
+        res.json(mess);
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // Get all inventory items
-app.get('/api/inventory', async (req, res) => {
+app.get("/api/inventory", async (req, res) => {
   try {
     const inventoryItems = await InventoryItem.find();
     res.json(inventoryItems);
@@ -40,17 +93,17 @@ app.get('/api/inventory', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-app.get('/', (req, res) => {
-  res.send('helo')
+app.get("/", (req, res) => {
+  res.send("helo");
 });
 
 // Get a single inventory item
-app.get('/api/inventory/:id', getInventoryItem, (req, res) => {
+app.get("/api/inventory/:id", getInventoryItem, (req, res) => {
   res.json(res.inventoryItem);
 });
 
 // Create a new inventory item
-app.post('/api/inventory', async (req, res) => {
+app.post("/api/inventory", async (req, res) => {
   const inventoryItem = new InventoryItem({
     number: req.body.number,
   });
@@ -63,7 +116,7 @@ app.post('/api/inventory', async (req, res) => {
 });
 
 // Update an existing inventory item
-app.patch('/api/inventory/:id', getInventoryItem, async (req, res) => {
+app.patch("/api/inventory/:id", getInventoryItem, async (req, res) => {
   if (req.body.number != null) {
     res.inventoryItem.number = req.body.number;
   }
@@ -76,10 +129,10 @@ app.patch('/api/inventory/:id', getInventoryItem, async (req, res) => {
 });
 
 // Delete an inventory item
-app.delete('/api/inventory/:id', getInventoryItem, async (req, res) => {
+app.delete("/api/inventory/:id", getInventoryItem, async (req, res) => {
   try {
     await res.inventoryItem.deleteOne();
-    res.json({ message: 'Deleted inventory item' });
+    res.json({ message: "Deleted inventory item" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -90,7 +143,7 @@ async function getInventoryItem(req, res, next) {
   try {
     const inventoryItem = await InventoryItem.findById(req.params.id);
     if (inventoryItem == null) {
-      return res.status(404).json({ message: 'Cannot find inventory item' });
+      return res.status(404).json({ message: "Cannot find inventory item" });
     }
     res.inventoryItem = inventoryItem;
     next();
@@ -100,4 +153,4 @@ async function getInventoryItem(req, res, next) {
 }
 
 // Start the server
-app.listen(3000, () => console.log('Server started'));
+app.listen(3000, () => console.log("Server started"));
